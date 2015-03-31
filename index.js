03d8603b22223be62478b1130dcef7a75b34d5c9
Joe Elsey <joe.elsey@gmail.com>
2015-03-30 21:06:03 -0700

    refactor done for now, no winner message still shows when player wins

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ var SlotMachine = function() {
     {number: null}, {number: 1, types: ''}, {number: 2, types: ''}, {number: 3, types: ''}, {number: 4, types: ''},
     {number: 5, types: ''}, {number: 6, types: ''}, {number: 7, types: ''}, {number: 8, types: ''}, {number: 9, types: ''}
   ];
+  this.rewardMarker = false;
 };
 
 SlotMachine.prototype.Turn = function() {
   console.log('this', this);
-  
+  //document.getElementById('message').innerHTML = '<div></div>';
+
   for (var i = 1; i <= 9; i++) {
     this.wheel[i].number = i;
     this.wheel[i].types = this.types[Math.floor(Math.random() * this.types.length)];
@@ -23,37 +25,54 @@ SlotMachine.prototype.Turn = function() {
 
   //reward generator.
   this.reward = function() {
+    this.wheelColumnOne();
+    this.wheelColumnTwo();
+    this.wheelColumnThree();
 
-    var rewardMarker = false;
+    if (this.rewardMarker === false) {
+      this.loseMoney();
+    } else {
+      this.rewardMarker = false;
+    }
+
+    if (this.wheelOne === false && this.wheelTwo === false && this.wheelThree === false) {
+      document.getElementById('message').innerHTML = '<div>No Winner.</div>';
+    }
+  };
+
+  this.wheelColumnOne = function() {
     this.wheelOne = false;
-    this.wheelTwo = false;
-    this.wheelThree = false;
 
-    if (this.wheel1 === this.wheel2 && this.wheel1 === this.wheel3) {
-      rewardMarker = true;
+    if (this.wheel[1].types === this.wheel[2].types && this.wheel[1].types === this.wheel[3].types) {
+      this.rewardMarker = true;
       this.wheelOne = true;
       this.addMoney();
-      this.winnerMessage(this.wheel1);
+      console.log('wheel column one type', this.wheel[1].types);
+      this.winnerMessage(this.wheel[1].types);
     }
+  };
 
-    if (this.wheel4 === this.wheel5 && this.wheel4 === this.wheel6) {
-      rewardMarker = true;
+  this.wheelColumnTwo = function() {
+    this.wheelTwo = false;
+
+    if (this.wheel[4].types === this.wheel[5].types && this.wheel[4].types === this.wheel[6].types) {
+      this.rewardMarker = true;
       this.wheelTwo = true;
       this.addMoney();
-      this.winnerMessage(this.wheel4);
+      console.log('wheel column two type', this.wheel[4].types);
+      this.winnerMessage(this.wheel[4].types);
     }
+  };
 
-    if (this.wheel7 === this.wheel8 && this.wheel7 === this.wheel9) {
-      rewardMarker = true;
+  this.wheelColumnThree = function() {
+    this.wheelThree = false;
+
+    if (this.wheel[7].types === this.wheel[8].types && this.wheel[7].types === this.wheel[9].types) {
+      this.rewardMarker = true;
       this.wheelThree = true;
       this.addMoney();
-      this.winnerMessage(this.wheel7);
-    }
-
-    if (rewardMarker === false) {
-      document.getElementById('message').innerHTML = '<div>No Winner.</div>';
-      console.log('No winner.');
-      this.loseMoney();
+      console.log('wheel column three type', this.wheel[7].types);
+      this.winnerMessage(this.wheel[7].types);
     }
   };
 
@@ -62,27 +81,27 @@ SlotMachine.prototype.Turn = function() {
     switch (wheel) {
       case 'Apple':
         console.log('Apples.  You win 1 dollar');
-        document.getElementById('message').innerHTML = '<div>Apples.  You win 1 dollar</div>';
+        document.getElementById('message').innerHTML += '<div>Apples.  You win 1 dollar</div>';
         break;
       case 'Cherry':
         console.log('Cherries. You win 3 dollars');
-        document.getElementById('message').innerHTML = '<div>Cherries.  You win 3 dollars</div>';
+        document.getElementById('message').innerHTML += '<div>Cherries.  You win 3 dollars</div>';
         break;
       case 'Orange':
         console.log('Oranges. You win 5 dollars');
-        document.getElementById('message').innerHTML = '<div>Oranges.  You win 5 dollars</div>';
+        document.getElementById('message').innerHTML += '<div>Oranges.  You win 5 dollars</div>';
         break;
       case 'Bell':
         console.log('Bells. You win 8 dollars');
-        document.getElementById('message').innerHTML = '<div>Bells.  You win 8 dollars</div>';
+        document.getElementById('message').innerHTML += '<div>Bells.  You win 8 dollars</div>';
         break;
       case '$':
         console.log('Dollars.  You win 16 dollars');
-        document.getElementById('message').innerHTML = '<div>Dollars.  You win 16 dollars</div>';
+        document.getElementById('message').innerHTML += '<div>Dollars.  You win 16 dollars</div>';
         break;
       case 'Jackpot':
         console.log('Jackpot!  You win the jackpot!');
-        document.getElementById('message').innerHTML = '<div>Jackpot.  You win the jackpot</div>';
+        document.getElementById('message').innerHTML += '<div>Jackpot.  You win the jackpot</div>';
         break;
       default:
         console.log('Error.');
@@ -91,22 +110,34 @@ SlotMachine.prototype.Turn = function() {
   };
 
   this.addMoney = function() {
-    if (this.wheel1 === 'Apple' && this.wheelOne === true || this.wheel4 === 'Apple' && this.wheelTwo === true || this.wheel7 === 'Apple' && this.wheelThree === true) {
+    if (this.wheel[1].types === 'Apple' && this.wheelOne === true ||
+        this.wheel[4].types === 'Apple' && this.wheelTwo === true ||
+        this.wheel[7].types === 'Apple' && this.wheelThree === true ) {
       this.money += 1;
     }
-    if (this.wheel1 === 'Cherry' && this.wheelOne === true || this.wheel4 === 'Cherry' && this.wheelTwo === true || this.wheel7 === 'Cherry' && this.wheelThree === true) {
+    if (this.wheel[1].types === 'Cherry' && this.wheelOne === true ||
+        this.wheel[4].types === 'Cherry' && this.wheelTwo === true ||
+        this.wheel[7].types === 'Cherry' && this.wheelThree === true) {
       this.money += 3;
     }
-    if (this.wheel1 === 'Orange' && this.wheelOne === true || this.wheel4 === 'Orange' && this.wheelTwo === true || this.wheel7 === 'Orange' && this.wheelThree === true) {
+    if (this.wheel[1].types === 'Orange' && this.wheelOne === true ||
+        this.wheel[4].types === 'Orange' && this.wheelTwo === true ||
+        this.wheel[7].types === 'Orange' && this.wheelThree === true) {
       this.money += 5;
     }
-    if (this.wheel1 === 'Bell' && this.wheelOne === true || this.wheel4 === 'Bell' && this.wheelTwo === true || this.wheel7 === 'Bell' && this.wheelThree === true) {
+    if (this.wheel[1].types === 'Bell' && this.wheelOne === true ||
+        this.wheel[4].types === 'Bell' && this.wheelTwo === true ||
+        this.wheel[7].types === 'Bell' && this.wheelThree === true) {
       this.money += 8;
     }
-    if (this.wheel1 === '$' && this.wheelOne === true || this.wheel4 === '$' && this.wheelTwo === true || this.wheel7 === '$' && this.wheelThree === true) {
+    if (this.wheel[1].types === '$' && this.wheelOne === true ||
+        this.wheel[4].types === '$' && this.wheelTwo === true ||
+        this.wheel[7].types === '$' && this.wheelThree === true) {
       this.money += 16;
     }
-    if (this.wheel1 === 'Jackpot' && this.wheelOne === true || this.wheel4 === 'Jackpot' && this.wheelTwo === true || this.wheel7 === 'Jackpot' && this.wheelThree === true) {
+    if (this.wheel[1].types === 'Jackpot' && this.wheelOne === true ||
+        this.wheel[4].types === 'Jackpot' && this.wheelTwo === true ||
+        this.wheel[7].types === 'Jackpot' && this.wheelThree === true) {
       this.money += 100;
     }
   };
@@ -120,9 +151,6 @@ SlotMachine.prototype.Turn = function() {
   };
 
   this.reward();
-  console.log(this.wheel1, this.wheel2, this.wheel3);
-  console.log(this.wheel4, this.wheel5, this.wheel6);
-  console.log(this.wheel7, this.wheel8, this.wheel9);
   document.getElementById('money').innerHTML = this.money;
 };
 

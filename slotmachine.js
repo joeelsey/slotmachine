51f5203e9e44850f6cc06a4ecbe144a3c0fde5ca
Joe Elsey <joe.elsey@gmail.com>
2015-04-26 16:55:05 -0700

    updated the slot machine to have more wheels and affected by bets and bank

--- slotmachine.js
+++ slotmachine.js
@@ -4,84 +4,72 @@ var Slotmachine = {
 
   wheels: [],
 
-  winnerBracket: [],
+  winnerBracket: [], //pushes the bracket that won into this array.  I feel like I'm going to need this in the future.
 
   bank: 100,
 
   bets: 0,
 
-  //main method.  passes a number of bets
+  //main method.  passes a number of bets. minimum of three.
   turn: function() {
     this.numberOfBets(3);
   },
 
   //takes the number of bets and passes that to createWheels, countAcrossRows, and adds to global bets
   numberOfBets: function(bets) {
-    this.createWheels(3);
+    this.createWheels(bets);
     this.countAcrossRows(bets);
     this.bets += bets;
+    this.bank -= bets;
+
+    console.log('Your bet: ',bets, 'Your bank: ', this.bank);
   },
 
+  //passes the number of wheels to create column and calls the function three times.
   createWheels: function(numberOfWheels) {
     for(var i = 0; i < 3; i++) {
       this.createColumn(numberOfWheels);
     }
   },
 
-  //creates wheels of fruit types
+  //creates a random fruit type
   createColumn: function(wheels) {
     for(var i = 0; i < wheels; i++) {
       this.wheels.push({number: this.wheels.length + 1, fruit: this.FRUIT[Math.floor(Math.random() * this.FRUIT.length)]});
     }
   },
 
+  //creates a row of fruit types by three
   countAcrossRows: function(rowNumber) {
-    var rowOneCounter = 0;
-    var rowTwoCounter = 3;
-    var rowThreeCounter = 6;
-
     var slotNumbersArray = [];
-    for(var i = 0; i < rowNumber; i++) {
-      rowOneCounter++;
-      rowTwoCounter++;
-      rowThreeCounter++;
+    for(var i = 0, j = 0, k = 3, l = 6; i < rowNumber; i++) {
+      j++;
+      k++;
+      l++;
 
-      slotNumbersArray.push({slotNumberOne: this.wheels[rowOneCounter - 1].fruit,
-                             slotNumberTwo: this.wheels[rowTwoCounter - 1].fruit,
-                             slotNumberThree: this.wheels[rowThreeCounter - 1].fruit});
+      slotNumbersArray.push({slotNumberOne: this.wheels[j - 1].fruit,
+                             slotNumberTwo: this.wheels[k - 1].fruit,
+                             slotNumberThree: this.wheels[l - 1].fruit});
     }
-    console.log('wheels', this.wheels);
-    console.log('slot number array', slotNumbersArray);
     this.winnerValidation(slotNumbersArray);
   },
 
+  //checks each row to see if there are matching fruit types.
   winnerValidation: function(slotNumbers) {
+    var winnerFruit = 'lose';
+    console.log('slotnumbers: ', slotNumbers);
     for(var i = 0; i < slotNumbers.length; i++) {
       console.log('Slotmachine: ',slotNumbers[i].slotNumberOne, slotNumbers[i].slotNumberTwo, slotNumbers[i].slotNumberThree);
       if(slotNumbers[i].slotNumberOne === slotNumbers[i].slotNumberTwo &&
          slotNumbers[i].slotNumberOne === slotNumbers[i].slotNumberThree) {
-           this.winnerMessage(slotNumbers[i].slotNumberOne);
-           this.winnerBracket.push({slotNumbers: i});
+           winnerFruit = slotNumbers[i].slotNumberOne;
+           this.winnerBracket.push({slotNumbers: i + 1});
          }
     }
-
-    return this.winnerBracket;
+    this.winnerMessage(winnerFruit);
+    console.log('winner bracket', this.winnerBracket);
   },
 
-  winnerReward: function(fruit) {
-    this.bank += this.bets;
-    console.log('this bank winner', this.bank, fruit);
-  },
-  //
-  // loserReward: function(bets) {
-  //   var loserMessage = 'no winner';
-  //   this.bank -= this.bets;
-  //
-  //   if(this.winnerBracket.length === 0) {
-  //     console.log(loserMessage);
-  //   }
-  // },
-  //
   winnerMessage: function(wheel) {
     switch (wheel) {
       case 'Apple':
@@ -108,9 +96,47 @@ var Slotmachine = {
         this.winnerReward('Jackpot');
         console.log('Jackpot!  You win the jackpot!');
         break;
+      case 'lose':
+        this.loserMessage();
+        break;
       default:
         console.log('Error.', wheel);
         break;
+    }
+  },
+
+  winnerReward: function(fruit) {
+    this.bank += this.bets;
+    switch (fruit) {
+      case 'Apple':
+        this.bank += 1;
+        break;
+      case 'Cherry':
+        this.bank += 3;
+        break;
+      case 'Orange':
+        this.bank += 5;
+        break;
+      case 'Bell':
+        this.bank += 8;
+        break;
+      case '$':
+        this.bank += 16;
+        break;
+      case 'Jackpot':
+        this.bank += 100;
+        break;
+      default:
+        console.log('there was an error in the winner reward switch');
+        break;
+    }
+    console.log('this bank winner', this.bank, fruit);
+  },
+
+  loserMessage: function() {
+    var loserMessage = 'no winner';
+    if(this.winnerBracket.length === 0) {
+      console.log(loserMessage);
     }
   }
 
